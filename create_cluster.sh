dec0d73b4b0f5f05265ed59fe068b3e36575107c
dev-ananta <ananta.dev@proton.me>
2026-02-15 17:06:49 -0600

    Commit 6: Define Locations for Edge-Native Architecture.

--- create_cluster.sh
+++ create_cluster.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+# Ensure Cluster is Deleted Before Creation if it Already Exists
+k3d cluster delete val-cloud
+
 # Cluster Creation
 k3d cluster create val-cloud \
     # -p 80:80@loadbalancer maps machine's port 80 to cluster's load balancer
     -p 80:80@loadbalancer \ 
     -p 443:443@loadbalancer \
-    # --k3s-arg disables default Traefik in order to install custom Traefik later (Optional, but recommended for better control)
-    --agents 2
+    -p "8080:80@server:0" \
+    --agents 2 \
+    --k3s-arg "--disable=traefik@server:0" \
+    # Adding Node Labels to Identify Home and Edge Nodes (Agent 0 is Home & Agent 1 is Edge)
+    --k3s-node-label "topology.kubernotes.io/zone=home@agent:0" \
+    --k3s-node-label "topology.kubernotes.io/zone=edge@agent:1"
 
 # Sending Confirmation Message
-echo "Cluster created! Switch context with:"
-echo "kubectl cluster-info"
+echo "Distributed Cluster Created."
+echo "Agent 0 is 'Home', Agent 1 is 'Edge'."
